e868a8b587ced587f4d62a27607aac285c7b31e6
Souvik Banerjee <souvik1997@gmail.com>
2015-06-19 15:29:49 -0500

    Add gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    coffeelint: {
+      app: ['*.coffee', 'src/**/.coffee', 'tests/**/*.coffee'],
+    },
+    watch: {
+      js: {
+        files: ['<%= jshint.files %>'],
+        tasks: ['jshint']
+      },
+      coffeescript: {
+        files: ['<%= coffeelint.files %>'],
+        tasks: ['coffeelint']
+      }
+    },
+    karma: {
+      unit: {
+        options: {
+          files: ['src/**/*.coffee', 'tests/**/*.coffee'],
+          preprocessors: {'**/*.coffee': ['coffee'] },
+          frameworks: ['jasmine'],
+          reporters: ['progress'],
+          port: 9876,
+          colors: true,
+          autoWatch: true,
+          browsers: ['Chrome', 'PhantomJS']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-coffeelint');
+  grunt.loadNpmTasks('grunt-karma');
+
+
+  grunt.registerTask('default', ['jshint', 'coffeelint']);
+
+};
